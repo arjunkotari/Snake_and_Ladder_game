5858219ddb3e79aa95cf37510ef6271a55eecab4
arjunkotari <arjun.kotari123@gmail.com>
2021-07-13 23:37:23 +0530

    Added: Player2

--- src/SnakeandLadderGame/SnakeandLadder.java
+++ src/SnakeandLadderGame/SnakeandLadder.java
@@ -1,4 +1,3 @@
-
 package SnakeandLadderGame;
 
 import java.util.Scanner;
@@ -6,49 +5,94 @@ import java.util.Scanner;
 public class SnakeandLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game play");
-		int player1 = 0;
-		int rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
-		System.out.println("Role die " + rollDie);
+		int player1 = 0,player2 = 0;
 		int snake,lader;
 		int count=0;
-//		int rollDie = 6;
 		Scanner sc = new Scanner(System.in);
-		if (rollDie == 6) {
-			System.out.println("Your game start now...");
-			player1 += rollDie;
-			System.out.println("Box position: " + player1);
-			
-			while (player1 != 100) 
+			while (player1 != 100 || player2 != 100) 
 			{
-				System.out.println("Press Enter to die roll ");
-				sc.nextLine();
+				
 				count++;
-				rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
-				System.out.println("Role die " + rollDie);
-				player1 += rollDie;
-				lader = (int) (1 + Math.floor(Math.random() * 10) % 6);
-				snake = (int) (1 + Math.floor(Math.random() * 10) % 6);
+				if(count%2==1)
+				{
+					System.out.println("Press Enter to die roll of player1");
+					sc.nextLine();
+					int rollDie1 = (int) (1 + Math.floor(Math.random() * 10) % 6);
+					System.out.println("Role die p1: " + rollDie1);
+					player1 += rollDie1;
+					lader = (int) (1 + Math.floor(Math.random() * 10) % 6);
+					snake = (int) (1 + Math.floor(Math.random() * 10) % 6);
 
-				//Lader
-				if(rollDie == lader)
-				{
-					player1 += rollDie;
-					System.out.println("Congrats, you got ladder...");					
+					//Lader
+					if(rollDie1 == lader)
+					{
+						player1 += rollDie1;
+						System.out.println("Congrats p1, you got ladder...");					
+					}
+					
+					//snake				
+					else if (rollDie1 == snake)
+					{
+						player1 -= rollDie1;
+						player1 -= rollDie1;
+						System.out.println("p1 Your attacked by snake...");
+					}
+					if(player1 == 100) {
+						System.out.println("p1 Box position: " + player1);
+						break;
+					}
+					if (player1 <= 100) {							
+						player1 += rollDie1;
+						player1 -= rollDie1;
+					}
+					else {				
+						player1 -= rollDie1;						
+					}
+					System.out.println("p1 Box position: " + player1);					
 				}
-				
-				//snake				
-				else if (rollDie == snake)
+				else
 				{
-					player1 -= rollDie;
-					player1 -= rollDie;
-					System.out.println("Your attacked by snake...");
+					System.out.println("Press Enter to die roll of player2 ");
+					sc.nextLine();
+					int rollDie2 = (int) (1 + Math.floor(Math.random() * 10) % 6);
+					System.out.println("Role die p2 " + rollDie2);
+					player2 += rollDie2;
+					lader = (int) (1 + Math.floor(Math.random() * 10) % 6);
+					snake = (int) (1 + Math.floor(Math.random() * 10) % 6);
+
+					//Lader
+					if(rollDie2 == lader)
+					{
+						player2 += rollDie2;
+						System.out.println("Congrats p2, you got ladder...");					
+					}
+					
+					//snake				
+					else if (rollDie2 == snake)
+					{
+						player2 -= rollDie2;
+						player2 -= rollDie2;
+						System.out.println("p2 Your attacked by snake...");
+					}
+					if(player2 == 100) {
+						System.out.println("p2 Box position: " + player2);
+						break;
+					}
+					else if (player2 <= 100) 								
+						System.out.println("p2 Box position: " + player2);				
+					else {
+						player2 -= rollDie2;
+						System.out.println("p1 Box position: " + player1);
+					}
 				}
-				
-				if (player1 <= 100) 								
-					System.out.println("Box position: " + player1);				
-				else				
-					player1 -= rollDie;	
 			}
+		//}
+		System.out.println();
+		if(player1 == 100) {
+			System.out.println("P1 own the game....");
+		}
+		else {
+			System.out.println("P2 own the game....");
 		}
 		System.out.println("Total dies Count: "+count);
 		sc.close();
