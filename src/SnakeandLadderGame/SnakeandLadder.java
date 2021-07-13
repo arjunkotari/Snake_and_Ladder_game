11d9d607d27086f2513469546ac11050692c5470
arjunkotari <arjun.kotari123@gmail.com>
2021-07-13 23:23:45 +0530

    updated: Winning position

--- src/SnakeandLadderGame/SnakeandLadder.java
+++ src/SnakeandLadderGame/SnakeandLadder.java
@@ -2,20 +2,23 @@ package SnakeandLadderGame;
 
 import java.util.Scanner;
 
-public class SnakeandLadder {
-	public static void main(String[] args) {
+public class SnakeandLadder 
+{
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome to Snake and Ladder game play");
 		int player1 = 0;
 		int rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
 		System.out.println("Role die " + rollDie);
 		int snake,lader;
 		Scanner sc = new Scanner(System.in);
-		if (rollDie == 6) {
+		if (rollDie == 6) 
+		{
 			System.out.println("Your game start now...");
 			player1 += rollDie;
 			System.out.println("Box position: " + player1);
 			
-			while (player1 >= 0) 
+			while (player1 <= 100) 
 			{
 				System.out.println("Press Enter to die roll ");
 				sc.nextLine();
@@ -38,11 +41,14 @@ public class SnakeandLadder {
 					player1 -= rollDie;
 					player1 -= rollDie;
 					System.out.println("Your attacked by snake...");
-				}							
-				System.out.println("Box position: " + player1);
+				}
+			System.out.println("Box position: " + player1);				
+				
 			}
+			sc.close();
 		}
-		sc.close();
 	}
 
 }
+
+
