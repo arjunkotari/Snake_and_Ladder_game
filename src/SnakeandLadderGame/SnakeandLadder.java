ff6fe34362e2fd4930ebfa1abee2ee91b46cd3f4
arjunkotari <arjun.kotari123@gmail.com>
2021-07-13 22:13:41 +0530

    Added: No play,Ladder or snake

--- src/SnakeandLadderGame/SnakeandLadder.java
+++ src/SnakeandLadderGame/SnakeandLadder.java
@@ -1,6 +1,5 @@
 package SnakeandLadderGame;
 
-
 import java.util.Scanner;
 
 public class SnakeandLadder {
@@ -9,5 +8,41 @@ public class SnakeandLadder {
 		int player1 = 0;
 		int rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
 		System.out.println("Role die " + rollDie);
+		int snake,lader;
+		Scanner sc = new Scanner(System.in);
+		if (rollDie == 6) {
+			System.out.println("Your game start now...");
+			player1 += rollDie;
+			System.out.println("Box position: " + player1);
+			
+			while (player1 >= 0) 
+			{
+				System.out.println("Press Enter to die roll ");
+				sc.nextLine();
+				rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
+				System.out.println("Role die " + rollDie);
+				player1 += rollDie;
+				lader = (int) (1 + Math.floor(Math.random() * 10) % 6);
+				snake = (int) (1 + Math.floor(Math.random() * 10) % 6);
+
+				//Lader
+				if(rollDie == lader)
+				{
+					player1 += rollDie;
+					System.out.println("Congrats, you got ladder...");					
+				}
+				
+				//snake				
+				else if (rollDie == snake)
+				{
+					player1 -= rollDie;
+					player1 -= rollDie;
+					System.out.println("Your attacked by snake...");
+				}							
+				System.out.println("Box position: " + player1);
+			}
+		}
+		sc.close();
 	}
+
 }
