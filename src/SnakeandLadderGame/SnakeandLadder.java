d1691e25b4ef7535c0b3ede53b11445c9c437f3a
arjunkotari <arjun.kotari123@gmail.com>
2021-07-13 22:03:32 +0530

    Added: Random to get number between 1 to 6

--- src/SnakeandLadderGame/SnakeandLadder.java
+++ src/SnakeandLadderGame/SnakeandLadder.java
@@ -1,10 +1,13 @@
 package SnakeandLadderGame;
 
+
 import java.util.Scanner;
 
 public class SnakeandLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game play");
-		int player1 = 0;  //single player
+		int player1 = 0;
+		int rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
+		System.out.println("Role die " + rollDie);
 	}
 }
