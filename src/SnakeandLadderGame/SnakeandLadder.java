c4484ad9a8b09351343909da5ee9876b218167f4
arjunkotari <arjun.kotari123@gmail.com>
2021-07-13 21:58:33 +0530

    Added: single player

--- src/SnakeandLadderGame/SnakeandLadder.java
+++ src/SnakeandLadderGame/SnakeandLadder.java
@@ -1,8 +1,10 @@
 package SnakeandLadderGame;
 
+import java.util.Scanner;
+
 public class SnakeandLadder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Snake and Ladder game play");
+		int player1 = 0;  //single player
 	}
-
 }
